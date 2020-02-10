3a000851a099f77d598e251abe5c2d2ed6a610ef
Daegun Choi <43776784+ChoiysApple@users.noreply.github.com>
2020-02-10 23:45:45 +0900

    Complete Xylophone Project

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -7,15 +7,74 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    // Button Clicked
+    @IBAction func CPressed(_ sender: UIButton) {
+        playSound(code: "C")
+    }
+    
+    @IBAction func DPressed(_ sender: UIButton) {
+        playSound(code: "D")
+    }
+    
+    @IBAction func EPressed(_ sender: UIButton) {
+        playSound(code: "E")
+    }
+    
+    @IBAction func FPressed(_ sender: UIButton) {
+        playSound(code: "F")
+    }
+    
+    @IBAction func GPressed(_ sender: UIButton) {
+        playSound(code: "G")
+    }
+    
+    @IBAction func APressed(_ sender: UIButton) {
+        playSound(code: "A")
+    }
+    
+    @IBAction func BPressed(_ sender: UIButton) {
+        playSound(code: "B")
+    }
     
 
+    func playSound(code: String) {
+        
+        // Check code validity
+        if ["C", "D", "E", "F", "G", "A", "B"].contains(code) {
+            print("Code "+code+" confirmed")
+        }
+        else {
+            print("Unable to find code")
+            return
+        }
+        
+        guard let url = Bundle.main.url(forResource: code, withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
