6578be868d4d432c860ac8c6b2ce1cfcb96204c9
Daegun Choi <43776784+ChoiysApple@users.noreply.github.com>
2020-02-12 02:49:04 +0900

    Add Button Click effect

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -21,31 +21,34 @@ class ViewController: UIViewController {
 
     // Button Clicked
     @IBAction func keyPressed(_ sender: UIButton) {
-
         playSound(note: sender.currentTitle!)
+        clickAnimation(target: sender)
     }
     
-    
-
-
-    
     func playSound(note: String) {
-        
         // Check note validity
-        if ["C", "D", "E", "F", "G", "A", "B"].contains(note) {
-            print("note "+note+" confirmed")
-        }
-        else {
-            print("Unable to find note")
-            return
-        }
+//        if ["C", "D", "E", "F", "G", "A", "B"].contains(note) {
+//            print("note "+note+" confirmed")
+//        }
+//        else {
+//            print("Unable to find note")
+//            return
+//        }
         
         // play sound
         let url = Bundle.main.url(forResource: note, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
     }
+    
+    // cahnge opacity and delay
+    func clickAnimation(target: UIButton){
+        target.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){}
+        target.alpha = 1
+    }
+    
+
 
 }
 
