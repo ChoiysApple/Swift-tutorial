a2b53e6facb2111934f188cf4c210d86aaba96e1
Daegun Choi <43776784+ChoiysApple@users.noreply.github.com>
2020-02-15 23:48:13 +0900

    Complete eggTImer

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,38 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // IB Connection
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var mainText: UILabel!
+
+    
     let hardnessTimes = ["Soft": 5, "Medium": 7, "Hard": 13]
     var timer: Timer?
     
-
     
     @IBAction func hardnessPressed(_ sender: UIButton) {
         let hardness = sender.currentTitle!
         
-        // reset timer
-        timer?.invalidate()
-        
-        //start Timer
-        startTimer(endTime: hardnessTimes[hardness]!)
-                
+        timer?.invalidate()                                 // reset timer
+        startTimer(endTime: hardnessTimes[hardness]!)       //start Timer
     }
     
 
     func startTimer(endTime: Int) {
         
         var currTime = 0
-        print("\n--- Count",endTime ,"---")
+//        print("\n--- Count",endTime ,"---")
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
             
-            print(endTime-currTime, "sec left")
+            self.progressBar.progress = Float(currTime)/Float(endTime)
+//            print(endTime-currTime, "sec left")
             currTime += 1
+            
 
             if currTime == endTime {
-                print("End timer")
+                self.progressBar.progress = 1.0
+                self.mainText.text = "Done!"
+//                print("End timer")
                 timer.invalidate()
             }
         }
-
-    }
+    }//[END] Timer
     
 }
