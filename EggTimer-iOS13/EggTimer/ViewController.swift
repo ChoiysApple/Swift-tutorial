07751549d9d5d411736d6524731c1309b0df8be6
Daegun Choi <43776784+ChoiysApple@users.noreply.github.com>
2020-02-15 23:26:59 +0900

    Add Timer

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -11,15 +11,37 @@ import UIKit
 class ViewController: UIViewController {
     
     let hardnessTimes = ["Soft": 5, "Medium": 7, "Hard": 13]
-    let timer = Timer()
+    var timer: Timer?
+    
 
     
     @IBAction func hardnessPressed(_ sender: UIButton) {
         let hardness = sender.currentTitle!
         
-        print(hardnessTimes[hardness]!)
+        // reset timer
+        timer?.invalidate()
         
+        //start Timer
+        startTimer(endTime: hardnessTimes[hardness]!)
+                
     }
     
+
+    func startTimer(endTime: Int) {
+        
+        var currTime = 0
+        print("\n--- Count",endTime ,"---")
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            
+            print(endTime-currTime, "sec left")
+            currTime += 1
+
+            if currTime == endTime {
+                print("End timer")
+                timer.invalidate()
+            }
+        }
+
+    }
     
 }
