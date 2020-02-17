8e5afd52976445490e170afef8d711c47bc48b01
Daegun Choi <43776784+ChoiysApple@users.noreply.github.com>
2020-02-17 23:15:00 +0900

    Add questionManager

--- Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueBtn: UIButton!
+    @IBOutlet weak var falseBtn: UIButton!
+    
+    var qnum = 0
+    
+    let question = [
+        ["Snail have teeth", "True"],
+        ["Fish can cough", "True"],
+        ["Tomato is vegitable","True"],
+        ["Potato is root of plant","False"],
+        ["Freddie Mucury was born in London", "False"]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+
     }
 
-
+    @IBAction func answerPressed(_ sender: UIButton) {
+        
+        if (qnum == question[0].count){
+            questionLabel.text = "Finished!"
+            return
+        }
+            
+        
+        quistionManager(questionNumber: qnum)
+        qnum += 1
+    }
+    
+    func quistionManager(questionNumber: Int){
+        questionLabel.text = question[questionNumber][0]
+    }
+    
+    
 }
 
