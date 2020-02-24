013e10427a489fd28624dff03347e1b6328dda66
ChoiysApple <chcs1370@gmail.com>
2020-02-24 20:28:12 +0900

    Add slider-responsive label

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
+
+    @IBAction func heightSliderMoved(_ sender: UISlider) {
+        heightLabel.text = String(format:"%.2f", sender.value)+"m"
+    }
+    
+    @IBAction func weightSliderMoved(_ sender: UISlider) {
+        weightLabel.text = String(format:"%.1f", sender.value)+"Kg"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
