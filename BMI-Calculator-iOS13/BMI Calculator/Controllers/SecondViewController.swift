c48aa7c260f6fc248cb1e406cd0c84ff5bae45a6
ChoiysApple <chcs1370@gmail.com>
2020-02-28 23:53:33 +0900

    Add label

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/SecondViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/SecondViewController.swift
@@ -12,6 +12,10 @@ class SecondViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let label = UILabel()
+        label.text = "Hello"
+        label.frame = CGRect(x: 0, y: 0, width: 100 , height: 50)
     }
 }
 
