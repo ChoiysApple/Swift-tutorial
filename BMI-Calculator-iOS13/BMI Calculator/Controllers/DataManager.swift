20c0bfe3e44db2b2c4bf88166d140d2d7bf1ea27
ChoiysApple <chcs1370@gmail.com>
2020-03-05 16:56:56 +0900

    Create DataManager

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/DataManager.swift
@@ -0,0 +1,17 @@
+//
+//  DataManager.swift
+//  BMI Calculator
+//
+//  Created by Daegeon Choi on 05/03/2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct DataManager{
+    
+    var height: Float = 1.5
+    var weight: Float = 100.0
+
+    setValues
+}
