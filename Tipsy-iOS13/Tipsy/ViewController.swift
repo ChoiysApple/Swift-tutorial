767f0a095fd0ca640d11733cf914746931a06f97
ChoiysApple <chcs1370@gmail.com>
2020-03-14 21:59:08 +0900

    get Pct value from button

--- Tipsy-iOS13/Tipsy/ViewController.swift
+++ Tipsy-iOS13/Tipsy/ViewController.swift
@@ -11,31 +11,57 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var billLabel: UITextField!
+    
     @IBOutlet weak var zeroPctBtn: UIButton!
     @IBOutlet weak var tenPctBtn: UIButton!
     @IBOutlet weak var twentyPctBtn: UIButton!
-    @IBOutlet weak var splitLabel: UILabel!
     
+    @IBOutlet weak var splitLabel: UILabel!
     
     var selectedBtn: UIButton?
     
+    var pct: Float = 0.0
+    var split: Float = 1.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // initialize pct buttons' status
         zeroPctBtn.isSelected = true
         tenPctBtn.isSelected = false
         twentyPctBtn.isSelected = false
-        
         selectedBtn = zeroPctBtn
+        
+        
     }
 
     @IBAction func pctBtnClicked(_ sender: UIButton) {
         selectedBtn?.isSelected = false
+        
+        if sender == zeroPctBtn{
+            pct = 0.0
+        }else if sender == tenPctBtn{
+            pct = 0.1
+        }else{
+            pct = 0.2
+        }
         sender.isSelected = true
         selectedBtn = sender
     }
     
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        print(sender)
+    }
+    
     @IBAction func CalculateBtnClicked(_ sender: UIButton) {
-        
+        print(billLabel.text!)
+        print(pct)
+        print(split)
+    }
+    
+    
+    func calculateTip(_ bill: Float, _ pct: Float, _ split: Float) -> Float{
+        return bill*pct/split
     }
     
     
