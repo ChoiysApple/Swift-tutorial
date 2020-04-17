2211b93d426d1ea7448a379aab67a5f9084b5909
ChoiysApple <chcs1370@gmail.com>
2020-04-17 17:17:59 +0900

    Add URL session & task

--- 7_Clima-iOS13/Clima/Model/WeatherDataManager.swift
+++ 7_Clima-iOS13/Clima/Model/WeatherDataManager.swift
@@ -9,26 +9,38 @@
 import Foundation
 
 struct WeatherDataManager{
-    let baseURL = "http://api.openweathermap.org/data/2.5/weather?appid=4e415e4ab2aaed09e04d8419beedee19&units=metric"
+    let baseURL = "https://api.openweathermap.org/data/2.5/weather?appid=4e415e4ab2aaed09e04d8419beedee19&units=metric"
     
     func fetchWeather(_ city: String){
         let completeURL = "\(baseURL)&q=\(city)"
         print(completeURL)
+        performRequest(url: completeURL )
     }
     
-    func preformRequest(url: String){
+    func performRequest(url: String){
         // 1. Create URL
         if let url = URL(string: url){          // URL initializer create optional URL
-            // 2. Create URL Session
+            
+            let session = URLSession(configuration: .default)                                                    // 2. Create URL Session
+            let task = session.dataTask(with: url, completionHandler: handler(data:response:error:))             // 3. Give the session a task
 
+            // what task do: go to url -> grab data -> come back
+            
+            // 4. Start the task
+            task.resume()
+        }
+    }
+    
+    func handler(data: Data?, response: URLResponse?, error: Error?){
+        if error != nil{
+            print(error!)
+            return
         }
         
-        
-        // 3. Give the session a task
-        
-        
-        // 4. Start the task
-        
-        
+        if let safeData = data{
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString ?? "data is nil")
+        }
     }
+    
 }
