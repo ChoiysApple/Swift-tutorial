ef739e7690fe7a7327155d57c0babbf37084be91
ChoiysApple <chcs1370@gmail.com>
2020-04-23 20:49:54 +0900

    Create WeatherData.swift

---
+++ 7_Clima-iOS13/Clima/Model/WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Daegeon Choi on 2020/04/23.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable{
+    // Value name must follow JSON property name
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable{
+    let temp: Double
+}
+
+struct Weather: Decodable{
+    let description: String
+}
