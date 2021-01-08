8e548fbe56cada71d1c1cd14b460cee83501825b
ChoiysApple <chcs1370@gmail.com>
2021-01-09 03:42:31 +0900

    Create CoinData.swift

---
+++ 7-1_ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,17 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Daegeon Choi on 2021/01/08.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+// Codable typealies: Protocal combined Decodable & Encodable
+struct CoinData: Codable{
+    // Value name must follow JSON property name
+    let asset_id_base: String
+    let asset_id_quote: String
+    let rate: Double
+}
