1370ddf4ff26ca373e09a02fed6df232ed3a48aa
ChoiysApple <chcs1370@gmail.com>
2021-01-13 14:20:47 +0900

    Setup UITableView

--- 8_Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ 8_Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -11,15 +11,29 @@ import Firebase
 
 class ChatViewController: UIViewController {
 
+    //MARK:- IBOutlets
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    
+    var messages: [Message] = [
+        Message(sender: "A", body: "Hi"),
+        Message(sender: "B", body: "Hello"),
+        Message(sender: "A", body: "How r u")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //MARK:- Navigation bar
+        //MARK: Navigation bar
         title = ID.appName
         navigationItem.hidesBackButton = true
+        
+        // tableview
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UINib(nibName: ID.cellNibName, bundle: nil), forCellReuseIdentifier: ID.cellIdentifier)
+        
     }
     
     @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
@@ -36,4 +50,24 @@ class ChatViewController: UIViewController {
     }
     
 
+}
+
+//MARK:- UITableView
+extension ChatViewController: UITableViewDataSource {
+    // numberOfRowsInSection
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ID.cellIdentifier, for: indexPath) as! MessageCell
+        cell.messageLabel?.text = "\(messages[indexPath.row].sender): \(messages[indexPath.row].body)"
+        return cell
+    }
+}
+
+extension ChatViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
 }
---
+++ 8_Flash-Chat-iOS13/Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Daegeon Choi on 2021/01/13.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
