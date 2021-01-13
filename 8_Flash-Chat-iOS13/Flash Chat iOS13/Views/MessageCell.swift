0787f6aae0109ca460b7a0a903d5445e1441255e
ChoiysApple <chcs1370@gmail.com>
2021-01-13 14:20:31 +0900

    Create MessageCell

---
+++ 8_Flash-Chat-iOS13/Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,29 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Daegeon Choi on 2021/01/13.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 2
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
