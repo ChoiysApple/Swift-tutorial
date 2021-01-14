4f5a47d619bf46ababcc302d39459ce1312bfa14
ChoiysApple <chcs1370@gmail.com>
2021-01-14 16:02:37 +0900

    Update IQKeyboardManager settings

--- 8_Flash-Chat-iOS13/Flash Chat iOS13/AppDelegate.swift
+++ 8_Flash-Chat-iOS13/Flash Chat iOS13/AppDelegate.swift
@@ -20,8 +20,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         
+        //MARK:IQKeyboardManager
         IQKeyboardManager.shared.enable = true
-
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
         return true
     }
 
