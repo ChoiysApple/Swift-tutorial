a7544384503bdf4d8d7540c5c0b6ec89a95b84d8
ChoiysApple <chcs1370@gmail.com>
2021-01-14 15:50:27 +0900

    Install IQKeyboardManager
    Used Swift Package Manager

--- 8_Flash-Chat-iOS13/Flash Chat iOS13/AppDelegate.swift
+++ 8_Flash-Chat-iOS13/Flash Chat iOS13/AppDelegate.swift
@@ -8,15 +8,19 @@
 
 import UIKit
 import Firebase
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        IQKeyboardManager.shared.enable = true
 
         return true
     }
