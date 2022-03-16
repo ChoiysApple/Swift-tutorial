6be7e07790725bd4b3470abb10ed82de7b7d029e
ChoiysApple <chcs1370@gmail.com>
2022-03-16 17:41:36 +0900

    Fix Appearance

--- Todoey-iOS13/Todoey/ToDoListViewController.swift
+++ Todoey-iOS13/Todoey/ToDoListViewController.swift
@@ -14,7 +14,7 @@ class ToDoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
     
     
